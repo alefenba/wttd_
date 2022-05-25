e0de3537311d3bfe1047defa3c36097b50e64b9d
alefenba <alefe.182@outlook.com>
2022-05-24 22:23:26 -0300

    Atualizado settings.py

--- eventex/settings.py
+++ eventex/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS',default='', cast=Csv)
+ALLOWED_HOSTS = ['127.0.0.1', '.localhost', '.herokuapp.com']
 
 
 # Application definition
