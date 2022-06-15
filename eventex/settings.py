8dba977823525e0d9ac9be91cb583a60daaf4479
alefenba <alefe.182@outlook.com>
2022-06-15 20:30:02 -0300

    Atualizando settings.py

--- eventex/settings.py
+++ eventex/settings.py
@@ -122,7 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
