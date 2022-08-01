10cd2718372b25458e25f0df50c09349ea7c01dd
alefenba <alefe.182@outlook.com>
2022-08-01 20:21:21 -0300

    Alterando envio de email smtp para o gmail

--- eventex/settings.py
+++ eventex/settings.py
@@ -153,6 +153,6 @@ DEFAULT_FROM_EMAIL = 'uchiha-itachi02@hotmail.com'
 EMAIL_BACKEND = config('EMAIL_BACKEND')
 EMAIL_HOST= config('EMAIL_HOST')
 EMAIL_PORT = config('EMAIL_PORT', cast=int)
-EMAIL_USE_TLS = config('EMAIL_USE_TLS',cast=bool)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_TLS = config('EMAIL_USE_TLS',cast=bool)
