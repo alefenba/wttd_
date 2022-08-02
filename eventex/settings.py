b679fd3c064c08dfa951f6f0706474525398dd2b
alefenba <alefe.182@outlook.com>
2022-08-01 21:03:18 -0300

    Alterado banco de dados

--- eventex/settings.py
+++ eventex/settings.py
@@ -83,8 +83,13 @@ WSGI_APPLICATION = 'eventex.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'eventex',
+        'USER': 'root',
+        'PASSWORD' : config('PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '3306',
+        
     }
 }
 
