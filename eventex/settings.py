62fc548ea9d8dc3b9458cf3e1798b9b5c17832e3
alefenba <alefe.182@outlook.com>
2022-08-01 21:44:51 -0300

    correção das config do Mysql

--- eventex/settings.py
+++ eventex/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'NAME': 'eventex',
         'USER': 'root',
         'PASSWORD' : config('PASSWORD', default = ''),
-        'HOST': 'localhost',
+        'HOST': '127.0.0.1',
         'PORT': '3306',
         
     }
